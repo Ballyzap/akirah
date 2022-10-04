42feab4880dfd6a205adf3c00729e9946e30a0d0
Ballyzap <114913524+Ballyzap@users.noreply.github.com>
2022-10-04 02:15:34 -0700

    Add files via upload

---
+++ test1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner Scan = new Scanner(System.in);
+        //to print 'Hello' and to print a name on a seperate line
+        System.out.println("hello");
+        System.out.println("input name");
+        String name = Scan.nextLine();
+    }
+}
